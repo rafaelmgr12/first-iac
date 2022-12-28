800aa639a84162efcbf78d1e66d9aa5a94f5d1dd
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-12-28 15:16:06 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0530ca8899fac469f"
+  instance_type = "t1.micro"
+
+  tags = {
+    Name = "Primeira Instância"
+  }
+}
